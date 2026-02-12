e1a0e1658df6022fac7efd92563c4b0198db4c4b
alexllenaf <alexllenaf@gmail.com>
2026-02-12 23:16:19 +0100

    Update interlena cask to v1.1.10 with arm64/x64 installers

--- Casks/interlena.rb
+++ Casks/interlena.rb
@@ -1,8 +1,15 @@
 cask "interlena" do
-  version "1.1.8"
-  sha256 "81d307b17c77bf8a27cff004431745fd0cadce1db70700e61fd4d666ba37e17f"
+  version "1.1.10"
+  on_arm do
+    sha256 "2926f52571ea26af49b5342b1fcb81b6e1a893ff727647d77a16c0829ad6206f"
+    url "https://github.com/alexllenaf/INTERLENA-updates/releases/download/v#{version}/Interview.Atlas-arm64.dmg"
+  end
 
-  url "https://github.com/alexllenaf/INTERLENA-updates/releases/download/v#{version}/Interview.Atlas.dmg"
+  on_intel do
+    sha256 "1711d69bb0251b7685e8b3f191b24b2a21fb8ab7776db398d2b07de0ecdb3533"
+    url "https://github.com/alexllenaf/INTERLENA-updates/releases/download/v#{version}/Interview.Atlas-x64.dmg"
+  end
+
   name "Interlena"
   desc "Local-first interview and application tracker"
   homepage "https://github.com/alexllenaf/INTERLENA-updates"
